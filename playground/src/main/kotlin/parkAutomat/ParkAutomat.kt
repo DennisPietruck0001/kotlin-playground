c7418e6f6816e1703acaa5a353bc53c5b1ff3454
Sorour Keramatboroujeni <sorour.keramatboroujeni@otto.de>
2022-12-05 16:19:26 +0100

    wieder TDD mit Aram

--- playground/src/main/kotlin/parkAutomat/ParkAutomat.kt
+++ playground/src/main/kotlin/parkAutomat/ParkAutomat.kt
@@ -1,4 +1,14 @@
 package parkAutomat
 
+import java.time.LocalDateTime
+
+
 class ParkAutomat {
+    fun pullTicket(KfZkennzeichen: String): Ticket {
+        return Ticket(KfZkennzeichen, LocalDateTime.now())
+    }
+
+    data class Ticket(val kennzeichen: String, val uhrzeit: LocalDateTime) {
+
+    }
 }
--- playground/src/test/kotlin/parkAutomat/ParkAutomatTest.kt
+++ playground/src/test/kotlin/parkAutomat/ParkAutomatTest.kt
@@ -1,5 +1,36 @@
 package parkAutomat
 
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
-internal class ParkAutomatTest
+internal class ParkAutomatTest {
+    @Test
+    fun `should return new Park Ticket`() {
+        val parkAutomat = ParkAutomat()
+
+        val ticket = parkAutomat.pullTicket("")
+
+        assertThat(ticket).isInstanceOf(ParkAutomat.Ticket::class.java)
+    }
+
+    @Test
+    fun `should set kennzeichen on new Ticket`() {
+        val parkAutomat = ParkAutomat()
+        val testKennzeichen = "HH-SK-1234"
+
+        val ticket = parkAutomat.pullTicket(testKennzeichen)
+
+        assertThat(ticket.kennzeichen).isEqualTo(testKennzeichen)
+    }
+
+    @Test
+    fun `should set uhrzeit on new Ticket`() {
+        val parkAutomat = ParkAutomat()
+
+        val ticket = parkAutomat.pullTicket("HH-SK-1234")
+
+        assertThat(ticket.uhrzeit).isBeforeOrEqualTo(LocalDateTime.now())
+    }
+}
